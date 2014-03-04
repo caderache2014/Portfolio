1f39258a38ecc7097d1816e1cf9df38ddecc9168
Christopher Billingham <cbillingham@gmail.com>
2014-03-03 17:20:38 -0800

    Rearranged gem file to accommodate sqlite and pg

---
+++ test/features/check_for_zurb_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+feature "check for Zurb" do 
+	scenario "check for columns" do
+		visit root_path
+		page.source.must_include "application.css"
+		page.body.must_include ".columns"
+	end
+end
